5982d0dbc53d823cc88d03a47284972e976f27f7
Ashley Ramsey <dusty1boy1984@gmail.com>
2022-12-15 18:44:59 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import "./App.css";
 
 function App() {
   const name = "Alice";
-  const element = <p>Hello, {name}!</p>
-  
+  const url = "https://www.google.com/";
+  const linkText = "Google";
+  const element = (
+    <p>
+      {" "}
+      Hey, {name}! Please visit <a href={url}>{linkText}</a>.
+    </p>
+  );
+
   return element;
 }
 
